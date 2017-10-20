44bbdca9fe12c32fcabc8e3046cdd4e5ff65034e
Brian Puschell <bpuschel@student.42.us.org>
2017-10-19 19:25:03 -0700

    Fixing ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bpuschel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 17:04:12 by bpuschel          #+#    #+#             */
-/*   Updated: 2017/10/19 18:36:10 by bpuschel         ###   ########.fr       */
+/*   Updated: 2017/10/19 19:24:37 by bpuschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 
 size_t	ft_strlcat(char *s1, const char *s2, size_t size)
 {
-	int	i;
-	int	j;
-	int size1;
-	int size2;
-	int	n;
+	int		i;
+	int		j;
+	size_t	size1;
+	size_t	size2;
+	int		n;
 
 	size1 = ft_strlen(s1);
 	size2 = ft_strlen(s2);
-	i = size1;
+	i = (int)size1;
 	j = -1;
-	n = size - size1;
+	n = (int)(size - size1);
+	if (size < size1 + 1)
+		return (size2 + size);
 	while (s2[++j] != '\0' && j < n)
 		s1[i++] = s2[j];
 	if (j == n && s2[j] != '\0')
